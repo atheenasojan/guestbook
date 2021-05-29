457ecf6f5e449e1cfd81c91f23610d7a335cffdb
atheenasojan <atheenasojan246@gmail.com>
2021-05-29 19:21:45 +0000

    changes made from theia lab environment

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-B64_URL=$(ibmcloud resource service-key "Auto-generated service credentials" --output json | jq .[0].credentials.url -j | base64 -w 0)
-B64_APIKEY=$(ibmcloud resource service-key "Auto-generated service credentials" --output json | jq .[0].credentials.apikey -j | base64 -w 0)
+B64_URL= $(ibmcloud resource service-key "https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/2bb5f643-23b6-4b64-ae9a-52da225cd6aa" --output json | jq .[0].credentials.url -j | base64 -w 0)
+B64_APIKEY= $(ibmcloud resource service-key "cip6v_Tgbl6Nm-81JFLD5w8XVY7uBLXByYGeDosfDjzw" --output json | jq .[0].credentials.apikey -j | base64 -w 0)
 
 cat <<EOF | oc apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
   name: binding-tone
-  namespace: <my_project>
+  namespace: sn-labs-moolayil13
 type: Opaque
 data:
   url: $B64_URL
